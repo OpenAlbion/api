720f4b9cdf21f19b6dfba4e8f328c46f88743f48
Pyae Sone Aung <44226349+PyaeSoneAungRgn@users.noreply.github.com>
2023-05-16 17:35:26 +0630

    wip

--- config/services.php
+++ config/services.php
@@ -34,7 +34,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/auth/google/callback',
+        'redirect' => 'https://api.openalbion.com/auth/google/callback',
     ],
 
     'wiki' => [
