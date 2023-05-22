ff5700abf18afba8369233801c3b62c06437b698
Pyae Sone Aung <44226349+PyaeSoneAungRgn@users.noreply.github.com>
2023-05-22 22:49:20 +0630

    wip

--- app/Http/Controllers/Api/Weaponry/SearchController.php
+++ app/Http/Controllers/Api/Weaponry/SearchController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api\Weaponry;
 
-use App\Enums\CategoryType;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Weaponry\SearchResource;
 use App\Models\Accessory;
