7e1f38c21f7abd033fcf641e9503f0df3ce04676
Pyae Sone Aung <44226349+PyaeSoneAungRgn@users.noreply.github.com>
2023-05-22 22:54:08 +0630

    wip

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Api Tokens') }}
                     </x-nav-link>
+                    <x-nav-link href="https://openalbion.com">
+                        {{ __('Documentation') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -69,6 +72,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Api Tokens') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link href="https://openalbion.com">
+                {{ __('Documentation') }}
             </x-responsive-nav-link>
         </div>
 
